9d81ae243369e8b54735ac85ae357945c6a4516f
akhror-valiev <ahror.valiev@gmail.com>
2023-01-12 14:56:15 +0500

    Update components

--- src/pages/Rocket.js
+++ src/pages/Rocket.js
@@ -1,32 +1,21 @@
 /* eslint-disable camelcase */
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+
 import { getRockets } from '../redux/rocketSlice';
-import RocketItem from '../components/RocketItem/RocketItem';
+
 import './Rocket.css';
 
 const Rocket = () => {
-  const dispatch = useDispatch();
-  const rockets = useSelector((state) => state.rockReducer);
-
   useEffect(() => {
-    dispatch(getRockets());
-  }, [dispatch]);
+    getRockets();
+  });
   return (
-    <ul className="rocket-ul">
-      {
-        rockets.map(({
-          flickr_images, rocket_name, description, id,
-        }) => (
-          <RocketItem
-            key={id}
-            flickr_images={flickr_images}
-            rocket_name={rocket_name}
-            description={description}
-          />
-        ))
-      }
-    </ul>
+
+    <>
+      <h1>Rocket</h1>
+      <h1>Rocket</h1>
+    </>
+
   );
 };
 
